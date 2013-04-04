93443c7152c1d751c186ed568f52bbf2af2e8bd7
gmit emp <mor@gmit.co.il>
2013-04-04 16:52:38 +0300

    added table filtering in logs page

--- js/logs_2.js
+++ js/logs_2.js
@@ -1,3 +1,49 @@
+	var sortSign = {
+		group: 1,
+		user: 1,
+		"log id": 1,
+		"log content" : 1
+	};
+	function getSortSign(key){
+		return sortSign[key];
+	}
+	function rowAtKey (row, key) {
+		var index = $(row).
+				parent().
+				parent().
+				find("thead th").
+				filter(function () {
+					return $(this).text() === key;
+				}).
+				index();
+		
+		var result = $(row).
+			find("td:nth-child("+ (index + 1) +")")[0]; 
+		//console.log($(row).find("td:nth-child(3)")[0]);
+		return result;
+	}
+	function sortTable (table, key) {
+		if (key.tagName && key.tagName === "TH") {
+			key = $(key).html();
+		}
+		if(!sortSign[key]){
+			sortSign[key] = 1;
+		}
+		$("tbody tr", table).
+				sort(function (aRow, bRow) {
+						var a = $(rowAtKey(aRow, key)).html(),
+						    b = $(rowAtKey(bRow, key)).html();
+						//console.log([ key,aRow,$(rowAtKey(aRow, key))]);
+						return  getSortSign(key) * (a > b ? 1 :
+								    a === b ? 0 :
+								    -1); 
+					}
+				).
+				remove().
+				appendTo("tbody",table);
+		sortSign[key] = -sortSign[key] ;
+	}
+
 $(document).ready(function () {
 	var tmp = {},
         plugins = {
@@ -13,6 +59,53 @@ $(document).ready(function () {
 							});
 			}
         };
+
+	$("#logs table thead th").on("click", function (e) { sortTable("#logs table", e.target)});
+
+	$("<select id=\"filterByThisColumn\">").
+		insertBefore("#logs").
+		append(
+			$("#logs table thead th").map(function () {
+				return  $("<option value=\""+ $(this).index() +"\">" + 
+						$(this).text()
+					+ "</option>")[0];
+			})
+		);
+	$("<input>").
+		attr("id", "filterByThisText").
+		attr("placeholder", "filter by this text").
+		insertAfter("#filterByThisColumn");
+	$("<button>filter</button>").
+		attr("id", "filterTheTable").
+		insertAfter("#filterByThisText").
+		on("click", function () {
+			var text = $("#filterByThisText").val();
+			$("#logs table tbody tr").
+				css("display", "none").
+				filter( function () {
+					var columnOption = $("#filterByThisColumn option:selected");
+					var columnIndex = columnOption.val();
+					var column = $(this).
+					               children().
+						       eq(columnIndex);
+					var findText = $("#filterByThisText").
+					           val();
+					return (findText === "") ||
+					       (column.has("dl").length > 0 && column.filter(':contains("'+findText+'")')).length > 0 ||
+					       (column.text() === findText);
+					//TODO if column has a definition-list, then if the definition-list contains findText ...
+				}).
+				css("display", "table-row");
+		});
+	$("#filterByThisText").keypress( function () {
+		setTimeout(
+			function () {
+				$("#filterTheTable").click();
+			},
+			10
+		);
+	});
+
 	function toListItem (obj) {
 		return function (val, key) {
 			return obj.hasOwnProperty(key) ? "<dt>" + key + "</dt><dd>"+val+"</dd>" : "";
